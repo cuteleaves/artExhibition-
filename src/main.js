1e76450976beb2868372d9d174f990a513d92ffd
Gumpgxd <3258694391@qq.com>
2021-06-30 10:32:08 +0800

    gxd 6.30

--- src/main.js
+++ src/main.js
@@ -5,12 +5,16 @@ import 'default-passive-events'
 // 手动配置element ui
 import ElementUI from 'element-ui'
 import store from '../src/store/index'
-//前面的￥massage是自己命名的，后面的Massage是组件名字
+//前面的$massage是自己命名的，后面的Massage是组件名字
 import 'element-ui/lib/theme-chalk/index.css'
 Vue.use(ElementUI)
 import axios from 'axios'
 Vue.prototype.$http = axios
 axios.defaults.baseURL = 'http://art.aztop.cn'
+axios.interceptors.request.use(config => {
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
 import { Message,MessageBox } from 'element-ui'
 Vue.prototype.$confirm = MessageBox.confirm
 Vue.prototype.$massage=Message
